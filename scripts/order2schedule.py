767cead696d7cbf6832154eed61de7d20b643b7e
Florian Kunneman <f.kunneman@gmail.com>
2016-07-18 17:11:12 +0200

    some progress

--- scripts/order2schedule.py
+++ scripts/order2schedule.py
@@ -89,61 +89,42 @@ for file in args.order_files:
                 title = rest.split(' ', 1)
             else:
                 title = rest
-            
-  #          print 'TITLE', title
             if re.search('\d+/TACL', title[-1]):
                 subconf_name = 'tacl'           
-   #             print 'SETTING SUBCONF TO TACL'
-#            elif re.search('\d+/short', title[-1]:
-#                subconf_name = 'short'
 
             if not sessions.has_key(session_name):
                 sessions[session_name] = Session("= %s %s" % (timerange, session_name), (day, date, year))
-
     
             sessions[session_name].add_paper(Paper(line, subconf_name))
-
-#for s in sessions.keys():
-#    print 'Session', sessions[s].date #, sessions[s].desc, sessions[s].chair 
-#    for p in sessions[s].papers:
-#        print 'Paper', p.id, p.time
-#quit()
 
 # Take all the sessions and place them at their time
 for session in sorted(sessions.keys()):
     day, date, year = sessions[session].date
     papers = sessions[session].papers
-    try:
+    if sessions[session].time == 'Session': # parallel session
         times_start = [datetime.strptime(p.time[:5], '%H:%M') for p in papers]
         times_end = [datetime.strptime(p.time[7:], '%H:%M') for p in papers]
         timestart = min(times_start).strftime('%I:%M')
         timeend = max(times_end).strftime('%I:%M')
         timerange = timestart + '--' + timeend
         
-        print(timerange)
-    except:
-        print 'error timerange'
-        continue
-
-    timeranges = schedule[(day, date, year)].keys()
-    if timerange in schedule[(day, date, year)].keys():
+        timeranges = schedule[(day, date, year)].keys()
+        if timerange in schedule[(day, date, year)].keys():
+            schedule[(day, date, year)][timerange].append(sessions[session])
+        else: # there might be blank slots
+            timestarts = [x[:5] for x in timeranges]
+            timeends = [x[7:] for x in timeranges]
+            if timestart in timestarts:
+                timeend = timeranges[timestarts.index(timestart)][7:]
+                timerange = timestart + '--' + timeend
+            elif timeend in timeends:
+                timestart = timeranges[timeends.index(timeend)][:5]
+                timerange = timestart + '--' + timeend
+            schedule[(day, date, year)][timerange].append(sessions[session])
+    else: # session already has timerange
+        timerange = sessions[session].time
         schedule[(day, date, year)][timerange].append(sessions[session])
-    else: # might be paper missing
-        timestarts = [x[:5] for x in timeranges]
-        timeends = [x[7:] for x in timeranges]
-        if timestart in timestarts:
-            timeend = timeranges[timestarts.index(timestart)][7:]
-            timerange = timestart + '--' + timeend
-        elif timeend in timeends:
-            timestart = timeranges[timeends.index(timeend)][:5]
-            timerange = timestart + '--' + timeend
-        schedule[(day, date, year)][timerange].append(sessions[session])
- #timerange = min(times)[:7] + maxsessions[session].time
-#    print >> sys.stderr, "SESSION", session, day, date, year, timerange
-#    if not schedule[(day, date, year)].has_key(timerange):
- #       schedule[(day, date, year)][timerange] = []
-  #  schedule[(day, date, year)][timerange].append(sessions[session])
-
+    
 def sort_times(a, b):
     ahour, amin = a[0].split('--')[0].split(':')
     bhour, bmin = b[0].split('--')[0].split(':')
@@ -165,22 +146,19 @@ def minus12(time):
 
 # Now iterate through the combined schedule, printing, printing, printing.
 # Write a file for each date. This file can then be imported and, if desired, manually edited.
+
 for date in dates:
-    print 'Date', date
     day, num, year = date
     for timerange, events in sorted(schedule[date].iteritems(), cmp=sort_times):
         start, stop = timerange.split('--')
 
-        #print('Events', timerange, events, dir(events))
         if not isinstance(events, list):
             continue
 
         parallel_sessions = filter(lambda x: isinstance(x, Session) and not x.poster, events)
         poster_sessions = filter(lambda x: isinstance(x, Session) and x.poster, events)
 
-        #print(len(parallel_sessions))
         # PARALLEL SESSIONS
-
         # Print the Session overview (single-page at-a-glance grid)
         if len(parallel_sessions) > 0:
             session_num = parallel_sessions[0].num
@@ -191,7 +169,8 @@ for date in dates:
             
             print >>out, '\\clearpage'
             print >>out, '\\setheaders{Session %s}{\\daydateyear}' % (session_num)
-            
+
+            print len(parallel_sessions), len(poster_sessions)            
             st = datetime(2016,8,7,int(start[:2]), int(start[3:])) # start time
             if len(parallel_sessions) == 2:
                 out1 = '\\begin{TwoSessionOverview}{Session %s}{\daydateyear}' % (session_num)
@@ -224,16 +203,11 @@ for date in dates:
                     t = '0' + t
                 print >>out, '  \\marginnote{\\rotatebox{90}{%s}}[2mm]' % (t)
                     
-                    #print 'paper_num', num_papers, paper_num
                 papers = []
-                print 'TIMES T', times, t
-                #st_str = st.strftime('%H:%M')
                 for session in parallel_sessions:
                     ts = [datetime.strptime(p.time[:5], '%H:%M').strftime('%02I:%02M') for p in session.papers]
- #                   print 'TS', ts, t
                     if t in ts:
                         p = session.papers[ts.index(t)]
-  #                      print 'T and ID', t, p.id
                         papers.append('\\papertableentry{%s}' % (p.id))
                     else:
                         papers.append('')
@@ -262,6 +236,7 @@ for date in dates:
         for session in poster_sessions:
             path = os.path.join(args.output_dir, '%s-%s.tex' % (day, session.name.replace(' ', '-')))
             out = open(path, 'w')
+            print 'POSTER', path
             print >> sys.stderr, "\\input{%s}" % (path)
 
             print >>out, '{\\section{%s}' % (session.name)
